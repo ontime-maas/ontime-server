63c1c161337425bc83d271c9c201d7db64c1f7cb
kys6879 <kys6879@naver.com>
2019-07-18 22:08:20 +0900

    api추가

--- routes/bus.js
+++ routes/bus.js
@@ -10,6 +10,93 @@ const tmoneyRequest = request.defaults({
     }
   })
 
+
+// 대중교통 정류장 검색
+// 안드로이드에서 정류장 arsId 가져오기
+router.get('/station', (req, response, next) => {
+    let stationName = req.query.stationName;
+    let stationId = req.query.stationId;
+
+    const options = {
+        uri: 'https://api.odsay.com/v1/api/searchStation',
+        qs: {
+          apiKey: "xLNnzzYE2gTHq/PiSt54nS1ag2uBq042bECGL6mq4PM",
+          lang: 0,
+          stationName: stationName
+        }
+      };
+    
+    request(options, (err, res, body) => {
+        if (err) {
+            response.json(err);
+        }
+        body = JSON.parse(body);
+        // response.json(body);
+        body.result.station.forEach(element => {
+            console.log(element);
+            if(element.stationID == stationId){
+                return response.json(element);
+            }
+        });
+    })
+});
+
+router.get('/station/arsid', (req, response, next) => {
+    let stationName = req.query.stationName;
+    let stationId = req.query.stationId;
+
+    const options = {
+        uri: 'https://api.odsay.com/v1/api/searchStation',
+        qs: {
+          apiKey: "xLNnzzYE2gTHq/PiSt54nS1ag2uBq042bECGL6mq4PM",
+          lang: 0,
+          stationName: stationName
+        }
+      };
+    
+    request(options, (err, res, body) => {
+        if (err) {
+            response.json(err);
+        }
+        body = JSON.parse(body);
+        // response.json(body);
+        body.result.station.forEach(element => {
+            console.log(element);
+            if(element.stationID == stationId){
+                
+                let ret = {
+                    arsID : element.arsID.split('-')[0]+element.arsID.split('-')[1]
+                }
+                return response.json(ret);
+            }
+        });
+    })
+});
+
+router.get('/station/arrive', (req, response, next) => {
+    let arsIdd = req.query.arsId;
+    let a = "I9IKiG3GwYwHbGGI9Aj9tALcoiJHVP7I6%2F58Lqog0dl7PMENlfwfUkAynzP%2BhWEUi%2FTOJsWbG6gT7pidnCI59w%3D%3D"
+    const options = {
+        uri: `http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?serviceKey=${a}&arsId=`+arsIdd,
+        // qs: {
+        //   serviceKey: "I9IKiG3GwYwHbGGI9Aj9tALcoiJHVP7I6%2F58Lqog0dl7PMENlfwfUkAynzP%2BhWEUi%2FTOJsWbG6gT7pidnCI59w%3D%3D",
+        //   arsId: arsIdd
+        // }
+      };
+
+      console.log(options.qs);
+    
+    request(options, (err, res, body) => {
+        
+        if (err) {
+            response.json(JSON.parse(err));
+        }
+        response.json(body);
+    })
+});
+
+
+
 // 주소에 대한 좌표를 얻기
 router.get('/:busNumber', function(req, res, next) {
     getBusByBusNumber(req.params.busNumber)
