a7da7d3580e560bf88964665416047fd84a49afd
kys6879 <kys6879@naver.com>
2019-07-20 12:55:19 +0900

    modify

---
+++ apikey.js
@@ -0,0 +1,7 @@
+const kakaoApiKey = "8a910942488ae9b7cc2ae09d61d381ff";
+const tmoneyApiKey = "349402ed-e696-4e05-ac9e-a796f5e616ab";
+module.exports = {
+    kakaoApiKey : kakaoApiKey,
+    tmoneyApiKey : tmoneyApiKey
+}
+
